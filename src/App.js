b6320baacd99107bff5050c42c62ea9325d2a4a5
MarindanyTracy <marindanytracy0@gmail.com>
2022-12-09 12:55:44 +0300

    Our Plans Section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Hero from './Components/Hero/Hero';
 import Programs from './Components/Programs/Programs';
 import Reasons from './Components/Reasons/Reasons';
+import Plans from './Components/Plans/Plans';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <Hero />
           <Programs />
           <Reasons />
+          <Plans />
     </div>
   );
 }
